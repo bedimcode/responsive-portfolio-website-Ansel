31b4efbd77346d5ca0da70728a63478738ce5118
bedimcode <61248995+bedimcode@users.noreply.github.com>
2022-01-22 10:28:29 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
