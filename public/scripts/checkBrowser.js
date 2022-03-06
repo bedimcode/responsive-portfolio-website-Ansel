e1aca5142eab2787f3c259886e2903d5b7d89778
MKAbuMattar <mohammad.khaled@outlook.com>
2022-03-06 14:17:35 +0300

    hotfix

--- public/scripts/checkBrowser.js
+++ public/scripts/checkBrowser.js
@@ -1,6 +1,12 @@
 // if browser is IE, redirect
 
-console.log(window.document.documentMode)
+if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+  window.location = 'microsoft-edge:' + window.location
+  setTimeout(function () {
+    window.open('', '_self', '').close()
+    window.location = '/outdated.html'
+  }, 0)
+}
 
 if (window.navigator.userAgent.indexOf('Trident/') > 0) {
   alert('Internet Explorer is not supported.')
